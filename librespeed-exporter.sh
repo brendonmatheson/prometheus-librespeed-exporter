c2ccfdb342d4b12bb45b4f471528bc14e68ca52c
Brendon Matheson <b@brendonmatheson.com>
2021-11-27 06:40:03 +0000

    Moved annotations to metrics

--- librespeed-exporter.sh
+++ librespeed-exporter.sh
@@ -6,19 +6,24 @@ for SERVER_ID in "${SERVER_ID_ARRAY[@]}"
 do
 
 	# Get the result
-	json=$(librespeed-cli --local-json librespeed-backends.json --server $SERVER_ID --json)
-	
+	JSON=$(librespeed-cli --local-json librespeed-backends.json --server $SERVER_ID --json)
+
 	# Parse out the values
-	server=$(echo $json | jq ".server.name")
-	bytes_sent=$(echo $json | jq ".bytes_sent")
-	bytes_received=$(echo $json | jq ".bytes_received")
-	ping=$(echo $json | jq ".ping")
-	jitter=$(echo $json | jq ".jitter")
-	upload=$(echo $json | jq ".upload")
-	download=$(echo $json | jq ".download")
-	
+	SERVER=$(echo $JSON | jq ".server.name")
+	BYTES_SENT=$(echo $JSON | jq ".bytes_sent")
+	BYTES_RECEIVED=$(echo $JSON | jq ".bytes_received")
+	PING=$(echo $JSON | jq ".ping")
+	JITTER=$(echo $JSON | jq ".jitter")
+	UPLOAD=$(echo $JSON | jq ".upload")
+	DOWNLOAD=$(echo $JSON | jq ".download")
+
 	# Produce the export line
-	echo "librespeed_result{server=$server,bytes_sent=\"$bytes_sent\",bytes_received=\"$bytes_received\",ping=\"$ping\",jitter=\"$jitter\",upload=\"$upload\",download=\"$download\"} 1"
-	
+	echo librespeed_bytes_sent{server=$SERVER} $BYTES_SENT
+	echo librespeed_bytes_received{server=$SERVER} $BYTES_RECEIVED
+	echo librespeed_ping{server=$SERVER} $PING
+	echo librespeed_jitter{server=$SERVER} $JITTER
+	echo librespeed_upload{server=$SERVER} $UPLOAD
+	echo librespeed_download{server=$SERVER} $DOWNLOAD
+
 done
 
